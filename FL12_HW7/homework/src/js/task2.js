bc1b735c358345c49380e0e65280c833ab0ff008
Diana Martyn <dianamartin790@gmail.com>
2019-12-29 18:03:23 +0200

    hw7 : refactoring

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,40 +1,47 @@
-var userNumber = 8;
+/* eslint-disable no-var */
 var amountOfTries = 3;
-var amountOfMoney=amountOfTries--;
-switch (amountOfMoney) {
-    case (amountOfTries = 3):
-        alert("Your prize is: 5$");
-        break
-    case (amountOfTries = 2):
-        alert("Your prize is: 50$");
-        break
-    case (amountOfTries = 1):
-        alert("Your prize is: 100$");
-        break
-    case (amountOfTries > 3):
-        alert("Your prize is: 0$");
-        break
-}
-// var randomNumber = Math.floor(Math.random() * 9);
-var randomNumber = 4;
+var amountOfMoney = 100;
+var maxNumber = 8;
+var randomNumber = Math.floor(Math.random() * (maxNumber + 1));
+var totalPrize = 0;
+
 // step 1
 if (confirm('Do you want to play a game?')) {
     while (amountOfTries) {
-        var str = `Choose a roulette pocket number from 0 to 8
-Attempts left: ${amountOfMoney}
-Total price: ${amountOfMoney}
-Possible prize on current attempt:${amountOfMoney}`;
+        var str = `Choose a roulette pocket number from 0 to ${maxNumber}
+Attempts left: ${amountOfTries}
+Total prize: ${totalPrize}
+Possible prize on current attempt:${amountOfMoney}
+NUMBER - ${randomNumber}`;
+
         var guessingNumber = Number(prompt(str));
+
         amountOfTries--;
-        if (!amountOfTries) {
-            alert(`Thank you for your participation. Your prize is:${amountOfMoney}`);
-            break
+
+        if (guessingNumber === randomNumber) {
+            totalPrize += amountOfMoney;
+            if (confirm(`Congratulation, you won!
+            Your prize is: ${totalPrize}$.
+            Do you want to continue?`)) {
+                amountOfTries = 3;
+                maxNumber += 4;
+                randomNumber = Math.floor(Math.random() * (maxNumber + 1));
+            } else {
+                amountOfTries = 0;
+            }
         }
-        if (guessingNumber === randomNumber)
-            (confirm(`Congratulation, you won!
-        Do you wnt to try again?
-        Your prize is:${amountOfMoney}`));
-        break
+
+        if (!amountOfTries) {
+            alert(`Thank you for your participation. Your prize is: ${totalPrize}$`);
+            if (confirm('Do you want to play again?')) {
+                totalPrize = 0;
+                amountOfTries = 3;
+                randomNumber = Math.floor(Math.random() * 9);
+            } else {
+                alert('Bye bye');
+                break;
+            }
+        }
     }
 } else {
     alert('You did not become a billionaire, but can.');
