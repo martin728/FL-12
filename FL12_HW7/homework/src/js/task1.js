49d2b9e329a7d9aad6e56c3727ceedacfed805b6
Diana Martyn <dianamartin790@gmail.com>
2019-12-29 18:47:58 +0200

    hw7

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -1,47 +1,49 @@
-let email = prompt("Enter your email", '');
+let email = prompt('Enter your email', '');
 const USER_EMAIL = 'user@gmail.com';
 const ADMIN_EMAIL = 'admin@gmail.com';
 const USER_PASSWORD = 'UserPass';
 const ADMIN_PASSWORD = 'AdminPass';
+const MIN_LENGTH = 6;
+const MAX_LENGTH = 5;
 
 
-var isUser = (email === USER_EMAIL);
-var isAdmin = (email === ADMIN_EMAIL);
+let isUser = email === USER_EMAIL
+let isAdmin = email === ADMIN_EMAIL;
 
 if (isUser || isAdmin) {
     let password = prompt('Password?', '');
-    if ((isAdmin && password === ADMIN_PASSWORD) || (isUser && password === USER_PASSWORD)) {
+    if (isAdmin && password === ADMIN_PASSWORD || isUser && password === USER_PASSWORD) {
 
-        if (confirm("Want to change password")) {
-            let oldPassword = prompt("Write the old password ", "");
+        if (confirm('Want to change password')) {
+            let oldPassword = prompt('Write the old password ', '');
             if (oldPassword === password) {
 
-                var newPassword = prompt("Enter a new password", "");
-                if (newPassword && newPassword.length > 6) {
-                    if (newPassword === prompt("Enter it again", "")) {
-                        alert("You have successfully changed your password.");
+                let newPassword = prompt('Enter a new password', '');
+                if (newPassword && newPassword.length > MIN_LENGTH) {
+                    if (newPassword === prompt('Enter it again', '')) {
+                        alert('You have successfully changed your password.');
                     } else {
-                        alert("You wrote the wrong password.");
+                        alert('You wrote the wrong password.');
                     }
-                } else if (newPassword && newPassword.length < 5) {
-                    alert("It’s too short password. Sorry.");
+                } else if (newPassword && newPassword.length < MAX_LENGTH) {
+                    alert('It’s too short password. Sorry.');
                 } else {
                     alert('Canceled');
                 }
 
             } else {
-                alert("Wrong old password");
+                alert('Wrong old password');
             }
         } else {
-            alert("You have failed the change.");
+            alert('You have failed the change.');
         }
     } else {
-        alert("Wrong password");
+        alert('Wrong password');
     }
 } else if (!email) {
     alert('Canceled');
-} else if (email.length < 5) {
-    alert("I don't know any emails having name length less than 5 symbols.");
+} else if (email.length < MAX_LENGTH) {
+    alert('I don`t know any emails having name length less than 5 symbols.');
 } else {
-    alert("I don't know you");
+    alert('I don`t know you');
 }
--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,44 +1,51 @@
-/* eslint-disable no-var */
-var amountOfTries = 3;
-var amountOfMoney = 100;
-var maxNumber = 8;
-var randomNumber = Math.floor(Math.random() * (maxNumber + 1));
-var totalPrize = 0;
+let amountOfTries = 3;
+const INITIAL_MAX_NUMBER = 8;
+const INITIAL_MAX_PRIZE = 100;
+let maxNumber = INITIAL_MAX_NUMBER;
+let randomNumber = Math.floor(Math.random() * (maxNumber + 1));
+let totalPrize = 0;
+let maxPrize = INITIAL_MAX_PRIZE;
+let amountOfMoneyOnCureentAttempt = maxPrize;
+const DECREASE_COEF = 2;
+const INCREASE_RANGE = 4;
+const MAX_TRIES = 3;
 
-// step 1
 if (confirm('Do you want to play a game?')) {
     while (amountOfTries) {
-        var str = `Choose a roulette pocket number from 0 to ${maxNumber}
+        let str = `Choose a roulette pocket number from 0 to ${maxNumber}
 Attempts left: ${amountOfTries}
 Total prize: ${totalPrize}
-Possible prize on current attempt:${amountOfMoney}
-NUMBER - ${randomNumber}`;
+Possible prize on current attempt:${amountOfMoneyOnCureentAttempt}`;
 
-        var guessingNumber = Number(prompt(str));
+        let guessingNumber = Number(prompt(str));
 
         amountOfTries--;
 
         if (guessingNumber === randomNumber) {
-            totalPrize += amountOfMoney;
+            totalPrize += amountOfMoneyOnCureentAttempt;
             if (confirm(`Congratulation, you won!
             Your prize is: ${totalPrize}$.
             Do you want to continue?`)) {
-                amountOfTries = 3;
-                maxNumber += 4;
+                maxPrize *= DECREASE_COEF;
+                amountOfMoneyOnCureentAttempt = maxPrize;
+                amountOfTries = MAX_TRIES;
+                maxNumber += INCREASE_RANGE;
                 randomNumber = Math.floor(Math.random() * (maxNumber + 1));
             } else {
                 amountOfTries = 0;
             }
+        } else {
+            amountOfMoneyOnCureentAttempt /= DECREASE_COEF;
         }
-
         if (!amountOfTries) {
             alert(`Thank you for your participation. Your prize is: ${totalPrize}$`);
             if (confirm('Do you want to play again?')) {
                 totalPrize = 0;
-                amountOfTries = 3;
-                randomNumber = Math.floor(Math.random() * 9);
+                amountOfTries = MAX_TRIES;
+                maxPrize = INITIAL_MAX_PRIZE;
+                amountOfMoneyOnCureentAttempt = maxPrize
+                randomNumber = Math.floor(Math.random() * (INITIAL_MAX_NUMBER + 1));
             } else {
-                alert('Bye bye');
                 break;
             }
         }
